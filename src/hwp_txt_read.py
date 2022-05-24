f88718723b729e0a664fbf107678acd3ffc6a951
Ktaewon <dydalehdxo@naver.com>
2022-05-25 03:12:30 +0900

    full-corpus added(hidden until assignment is ended), [hwp_txt_read.py]: read corpus data fucntion is added, [main2.py]: sorting(temp) and corpus read function is reflected

---
+++ src/hwp_txt_read.py
@@ -0,0 +1,85 @@
+import os
+import olefile
+# from bs4 import BeautifulSoup
+# import xml.etree.ElementTree as ET
+
+def readHWPandParse(filename:str):
+    # os.getcwd() + '/input/full_corpus.hwp'
+    f = olefile.OleFileIO(filename) #olefile로 한글파일 열기
+    encoded_text = f.openstream('PrvText').read() #PrvText 스트림 안의 내용 꺼내기 (유니코드 인코딩 되어 있음)
+    # print(encoded_text)
+    
+    # for encoded_text in f.openstream('PrvText').readlines():
+    #     print(encoded_text)
+    #     print(encoded_text.decode('utf-16'))
+    decoded_text = encoded_text.decode('UTF-16') #유니코드이므로 UTF-16으로 디코딩
+    
+    return decoded_text
+
+def readTXTandParseAsDict(filename:str):
+    corpus_dict = {}
+    with open(filename, "r", encoding="utf-8") as f:
+        lines = f.readlines()
+        count = 0
+        tempbody = ""
+        title_find = False
+        for line in lines:
+            if line.find("<title>") >= 0:
+                if title_find == True:
+                    corpus_dict[count] = tempbody
+                    tempbody = ""
+                else:
+                    title_find = True
+                count += 1
+                line = line.replace("<title>", "")
+                line = line.replace("</title>", "")
+            if not line == "" and not line == "\n":
+                tempbody += line
+        corpus_dict[count] = tempbody
+    #print(corpus_dict)
+    return corpus_dict
+
+def readTXTandParseAsList(filename:str):
+    corpus_list = [""]
+    with open(filename, "r", encoding="utf-8") as f:
+        lines = f.readlines()
+        count = 0
+        tempbody = ""
+        title_find = False
+        for line in lines:
+            if line.find("<title>") >= 0:
+                if title_find == True:
+                    corpus_list.append(tempbody)
+                    tempbody = ""
+                else:
+                    title_find = True
+                line = line.replace("<title>", "")
+                line = line.replace("</title>", "")
+            if not line == "" and not line == "\n":
+                tempbody += line
+        corpus_list.append(tempbody)
+    #print(corpus_dict)
+    return corpus_list
+
+
+corpus = readTXTandParseAsList(os.getcwd() + '/input/full_corpus.txt')
+for i in range(len(corpus)):
+    print("[{}] : {}".format(i, corpus[i]))
+    
+# decoded_text = readHWPandParse(os.getcwd() + '/input/full_corpus.hwp')
+# print(decoded_text)
+
+
+# tree = ET.fromstring(decoded_text)
+
+# root = tree.getroot() # 해당 트리의 root를 반환
+# print(root.tag)
+# print(root.attrib) 
+
+# for child in root:
+#     print(child.tag, child.attrib)
+# bs = BeautifulSoup(decoded_text, 'html.parser')
+# print(bs)
+
+# 5번 글 /안붙어있었음
+# print(bs.find_all('title'))
--- src/main2.py
+++ src/main2.py
@@ -1,6 +1,7 @@
 from konlpy.tag import Mecab
 from math import log10
 import os
+from hwp_txt_read import readTXTandParseAsList
 
 mecab = Mecab(dicpath=r"C:\mecab\mecab-ko-dic")
 
@@ -92,7 +93,7 @@ def makePostingList(D): # D : CORPUS
 # term-at-a-time 방식의 cosine 점수 계산 함수
 def consineScore(q, D, term_dict):
     N = len(D)
-    scores = [0 for i in range(N)]
+    scores = {i: 0 for i in range(N)}
     length = [0 for i in range(N)]
     query_terms = mecab.morphs(q)
     for t in set(query_terms):
@@ -114,23 +115,27 @@ def consineScore(q, D, term_dict):
         
 
 # 문서 입력
-CORPUS = ["""<title>1. 지미 카터</title>
-제임스 얼 지미 카터 주니어는 민주당 출신 미국 39번째 대통령이다. 지미 카터는 조지아 주 한 마을에서 태어났다. 조지아 공과대학교를 졸업하였다. 그 후 해군에 들어가 전함·원자력·잠수함의 승무원으로 일하였다.""",
-"""<title>2. 체첸 공화국</title> 
-체첸 공화국 또는 줄여서 체첸은 러시아의 공화국이다. 체첸에서 사용되는 언어는 체첸어와 러시아어이다. 체첸어는 캅카스제어 중, 북동 캅카스제어로 불리는 그룹에 속하는데 인구시어와 매우 밀접한 관계에 있다.
-""",
-"""<title>3. 백남준</title> 
-백남준은 한국 태생의 미국 미술작가, 작곡가, 전위 예술가이다. 여러 가지 매체로 예술 활동을 하였고 특히 비디오 아트라는 새로운 예술을 창안하여 발전시켰다는 평가를 받는 예술가로서 '비디오 아트의 창시자'로 알려져 있다. 
-"""
-]
+CORPUS = readTXTandParseAsList(os.getcwd() + '/input/full_corpus.txt')
+# CORPUS = ["""<title>1. 지미 카터</title>
+# 제임스 얼 지미 카터 주니어는 민주당 출신 미국 39번째 대통령이다. 지미 카터는 조지아 주 한 마을에서 태어났다. 조지아 공과대학교를 졸업하였다. 그 후 해군에 들어가 전함·원자력·잠수함의 승무원으로 일하였다.""",
+# """<title>2. 체첸 공화국</title> 
+# 체첸 공화국 또는 줄여서 체첸은 러시아의 공화국이다. 체첸에서 사용되는 언어는 체첸어와 러시아어이다. 체첸어는 캅카스제어 중, 북동 캅카스제어로 불리는 그룹에 속하는데 인구시어와 매우 밀접한 관계에 있다.
+# """,
+# """<title>3. 백남준</title> 
+# 백남준은 한국 태생의 미국 미술작가, 작곡가, 전위 예술가이다. 여러 가지 매체로 예술 활동을 하였고 특히 비디오 아트라는 새로운 예술을 창안하여 발전시켰다는 평가를 받는 예술가로서 '비디오 아트의 창시자'로 알려져 있다. 
+# """
+# ]
 
 # 토큰나이징 & 역인덱싱 & tf-idf weight 계산
 term_dict = makePostingList(CORPUS)
 printPostingList(term_dict)
 
 # 쿼리 들어옴
-query = ["체첸 공화국 만세 체첸 러시아 민주당에서 예술활동, 아티스트"]
+query = ["체첸 공화국 만세 체첸 러시아 민주당에서 예술가, 아티스트, 작곡가"]
 
 # 쿼리 토크나이징 & 점수 계산
 scores =consineScore(query[0], CORPUS, term_dict)
+
+
+print(sorted(scores.items(), key = lambda item: item[1], reverse = True)[:5])
 
