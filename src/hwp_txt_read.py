f802f2e8f638d252ff1a9d989b69aee2c37057af
Ktaewon <dydalehdxo@naver.com>
2022-06-06 23:10:08 +0900

    '[hwp_txt_read.py]: hwp 파일 그대로 읽어 list로 반환하는 함수 완료

--- src/hwp_txt_read.py
+++ src/hwp_txt_read.py
@@ -1,20 +1,84 @@
 import os
 import olefile
+import zlib
+import struct
 # from bs4 import BeautifulSoup
 # import xml.etree.ElementTree as ET
 
-def readHWPandParse(filename:str):
+def readHWP(filename:str):
     # os.getcwd() + '/input/full_corpus.hwp'
-    f = olefile.OleFileIO(filename) #olefile로 한글파일 열기
-    encoded_text = f.openstream('PrvText').read() #PrvText 스트림 안의 내용 꺼내기 (유니코드 인코딩 되어 있음)
-    # print(encoded_text)
+    f = olefile.OleFileIO(filename) #olefile로 .hwp 파일 열기
     
-    # for encoded_text in f.openstream('PrvText').readlines():
-    #     print(encoded_text)
-    #     print(encoded_text.decode('utf-16'))
-    decoded_text = encoded_text.decode('UTF-16') #유니코드이므로 UTF-16으로 디코딩
+    # OLE 파일이 가진 stream 리스트
+    dirs = f.listdir()
+
+    # hwp 파일인지 확인
+    if ["FileHeader"] not in dirs or ["\x05HwpSummaryInformation"] not in dirs:
+        raise Exception("Not a HWP format.")
     
-    return decoded_text
+    # 문서 포맷 압축 여부 확인
+    header_data = f.openstream("FileHeader").read()
+    is_compressed = (header_data[36] & 1) == 1
+    
+    # Body Sections 불러오기
+    nums = []
+    for d in dirs:
+        if d[0] == "BodyText":
+            nums.append(int(d[1][len("Section"):]))
+    sections = ["BodyText/Section" + str(x) for x in sorted(nums)]
+    
+    # 전체 text 추출
+    text = ""
+    for section in sections:
+        encoded_bodytext = f.openstream(section)
+        encoded_data = encoded_bodytext.read()
+        if is_compressed: # 압축된 경우 해제
+            unpacked_encoded_data = zlib.decompress(encoded_data, -15)
+        else:
+            unpacked_encoded_data = encoded_data
+    
+        # 각 Section 내 text 추출    
+        section_text = ""
+        i = 0
+        size = len(unpacked_encoded_data)
+        while i < size:
+            header = struct.unpack_from("<I", unpacked_encoded_data, i)[0]
+            rec_type = header & 0x3ff
+            rec_len = (header >> 20) & 0xfff
+            if rec_type in [67]:
+                rec_data = unpacked_encoded_data[i+4:i+4+rec_len]
+                section_text += rec_data.decode('utf-16')
+                section_text += "\n"
+            i += 4 + rec_len
+        text += section_text
+        text += "\n"
+    
+    f.close()
+    # 디코딩된 text 반환
+    return text
+
+
+def readHWPandParseAsList(filename:str):
+    corpus_list = [""]
+    # os.getcwd() + '/input/full_corpus.hwp'
+    lines = readHWP(filename).strip().split('\r\n')
+    print(lines)
+    tempbody = ""
+    title_find = False
+    for line in lines:
+        if line.find("<title>") >= 0:
+            if title_find == True:
+                corpus_list.append(tempbody)
+                tempbody = ""
+            else:
+                title_find = True
+            line = line.replace("<title>", "")
+            line = line.replace("</title>", "")
+        if not line == "" and not line == "\n":
+            tempbody += f'{line}\n'
+    corpus_list.append(tempbody)
+    
+    return corpus_list
 
 def readTXTandParseAsDict(filename:str):
     corpus_dict = {}
@@ -62,7 +126,7 @@ def readTXTandParseAsList(filename:str):
     return corpus_list
 
 def main():
-    corpus = readTXTandParseAsList(os.getcwd() + '/input/full_corpus.txt')
+    corpus = readHWPandParseAsList(os.getcwd() + '/input/full_corpus.hwp')
     for i in range(len(corpus)):
         print("[{}] : {}".format(i, corpus[i]))
     
